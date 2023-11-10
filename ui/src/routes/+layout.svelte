dae6a9363041ce008c200cfc87bfdd30f468fba4
Ethan Look <ethan.look@viam.com>
2023-11-10 12:54:09 -0500

    Add boilerplate SvelteKit UI

---
+++ ui/src/routes/+layout.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+import '@viamrobotics/prime-core/prime.css';
+</script>
+
+<slot />
---
+++ ui/src/routes/+page.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+  import { Banner } from "@viamrobotics/prime-core";
+</script>
+
+<Banner variant="info">
+  <svelte:fragment slot="title">Airbot</svelte:fragment>
+  <svelte:fragment slot="message">Coming soon</svelte:fragment>
+</Banner>
